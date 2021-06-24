92057b945d97afd99fe620a4d6d5cae79caf9db9
doyle1996 <1184125133@qq.com>
2021-06-24 12:21:37 +0800

    Spring整合Drools+web

---
+++ src/main/java/com/zjb/service/RuleService.java
@@ -0,0 +1,18 @@
+package com.zjb.service;
+
+import org.kie.api.KieBase;
+import org.kie.api.cdi.KBase;
+import org.kie.api.runtime.KieSession;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RuleService {
+    @KBase("kbase")
+    private KieBase kieBase;
+   public void rule(){
+       KieSession kieSession=kieBase.newKieSession();
+       kieSession.fireAllRules();
+       kieSession.dispose();
+   }
+
+}
